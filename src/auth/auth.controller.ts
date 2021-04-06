2db47fc55c0b760c4f3800696206d4debeb44b05
krung2 <jungbin4337@dgsw.hs.kr>
2021-04-06 16:05:18 +0900

    Fix: return type

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, HttpCode, Post, Query } from '@nestjs/common';
+import { Body, Controller, HttpCode, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import User from 'src/entities/user.entity';
+import { Token } from 'src/lib/decorator/token.decorator';
 import returnLib from 'src/lib/return.lib';
 import * as tokenLib from 'src/lib/token.lib';
+import AuthGaurd from 'src/middleware/auth.middleware';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
@@ -30,5 +33,15 @@ export class AuthController {
     const token = await tokenLib.generateKey(user.id, user.name, user.userKey);
 
     return returnLib(200, '로그인 성공', {user, token});
+  }
+
+  @Patch('profile')
+  // tslint:disable-next-line: new-parens
+  @UseGuards(new AuthGaurd(1))
+  async modifyProfile (
+    @Token() user: User,
+    @Body() profileImage: string,
+  ) {
+      await this.authService.modifyProfile(user.id, profileImage);
   }
 }
